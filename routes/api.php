5e4d86398d6f84ac94eb6b6cf1c8f2844e4e7b6c
NShubart <nshubart@gmail.com>
2023-12-08 13:46:41 +0200

    disable categories

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ use App\Http\Controllers\Admin\ProductController;
     Route::get('product', [ProductController::class, 'index']); //{
     // If the Content-Type and Accept headers are set to 'application/json',
     // this will return a JSON structure. This will be cleaned up later.
-    return ProductService::all();
+   // return ProductService::all();
     //});
 
 
